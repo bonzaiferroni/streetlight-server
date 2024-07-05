0f5a48e0c7e131bfb5ab2c20153c74f3afa4e461
Luke Bollwerk <lukas8@gmail.com>
2024-07-05 15:39:30 -0600

    reordering

--- src/main/kotlin/streetlight/server/db/services/EventTable.kt
+++ src/main/kotlin/streetlight/server/db/services/EventTable.kt
@@ -5,6 +5,14 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 
+object EventTable : IntIdTable() {
+    val user = reference("user_id", UserTable)
+    val location = reference("location_id", LocationTable)
+    val timeStart = long("time_start")
+    val hours = float("hours")
+    val url = text("url").nullable()
+}
+
 class EventEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : EntityClass<Int, EventEntity>(EventTable)
 
@@ -13,12 +21,4 @@ class EventEntity(id: EntityID<Int>) : IntEntity(id) {
     var timeStart by EventTable.timeStart
     var hours by EventTable.hours
     var url by EventTable.url
-}
-
-object EventTable : IntIdTable() {
-    val user = reference("user_id", UserTable)
-    val location = reference("location_id", LocationTable)
-    val timeStart = long("time_start")
-    val hours = float("hours")
-    val url = text("url").nullable()
 }
