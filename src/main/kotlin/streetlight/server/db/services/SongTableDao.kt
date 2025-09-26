04cef239421c9b37c770a73860d03db46cb95ec2
Luke Bollwerk <lukas8@gmail.com>
2025-09-26 17:17:49 -0600

    create live event dashboards

--- src/main/kotlin/streetlight/server/db/services/SongTableDao.kt
+++ src/main/kotlin/streetlight/server/db/services/SongTableDao.kt
@@ -6,12 +6,15 @@ import klutch.db.read
 import klutch.utils.eq
 import klutch.utils.toStringId
 import klutch.utils.toUUID
+import kotlinx.datetime.Clock
 import org.jetbrains.exposed.sql.insertAndGetId
 import streetlight.model.data.NewSong
+import streetlight.model.data.Song
 import streetlight.model.data.SongId
 import streetlight.model.data.toProjectId
 import streetlight.server.db.tables.SongTable
 import streetlight.server.db.tables.toSong
+import streetlight.server.db.tables.writeFull
 
 class SongTableDao: DbService() {
     suspend fun readSongs(userId: UserId) = dbQuery {
@@ -21,9 +24,16 @@ class SongTableDao: DbService() {
 
     suspend fun createSong(userId: UserId, newSong: NewSong): SongId = dbQuery {
         SongTable.insertAndGetId {
-            it[this.userId] = userId.toUUID()
-            it[this.name] = newSong.name
-            it[this.artist] = newSong.artist
+            it.writeFull(Song(
+                songId = SongId.random(),
+                userId = userId,
+                title = newSong.title,
+                artist = newSong.artist,
+                tempo = null,
+                capo = null,
+                music = null,
+                createdAt = Clock.System.now(),
+            ))
         }.value.toStringId().toProjectId()
     }
 }
--- src/main/kotlin/streetlight/server/db/tables/SongTable.kt
+++ src/main/kotlin/streetlight/server/db/tables/SongTable.kt
@@ -1,10 +1,13 @@
 package streetlight.server.db.tables
 
+import kabinet.utils.toInstantFromUtc
+import kabinet.utils.toLocalDateTimeUtc
 import klutch.db.tables.UserTable
 import klutch.utils.toUUID
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 import org.jetbrains.exposed.sql.statements.UpdateBuilder
 import streetlight.model.data.Song
 import streetlight.server.utils.toProjectId
@@ -15,6 +18,9 @@ object SongTable : UUIDTable() {
     val name = text("title")
     val artist = text("artist").nullable()
     val music = text("music").nullable()
+    val tempo = integer("tempo").nullable()
+    val capo = integer("capo").nullable()
+    val createdAt = datetime("created_at")
 }
 
 fun ResultRow.toSong() = Song(
@@ -23,12 +29,16 @@ fun ResultRow.toSong() = Song(
     title = this[SongTable.name],
     artist = this[SongTable.artist],
     music = this[SongTable.music],
+    tempo = this[SongTable.tempo],
+    capo = this[SongTable.capo],
+    createdAt = this[SongTable.createdAt].toInstantFromUtc(),
 )
 
 // Updaters
 fun UpdateBuilder<*>.writeFull(song: Song) {
     this[SongTable.id] = song.songId.toUUID()
     this[SongTable.userId] = song.userId.toUUID()
+    this[SongTable.createdAt] = song.createdAt.toLocalDateTimeUtc()
     writeUpdate(song)
 }
 
@@ -36,4 +46,6 @@ fun UpdateBuilder<*>.writeUpdate(song: Song) {
     this[SongTable.name] = song.title
     this[SongTable.artist] = song.artist
     this[SongTable.music] = song.music
+    this[SongTable.tempo] = song.tempo
+    this[SongTable.capo] = song.capo
 }
