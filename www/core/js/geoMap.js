720a792319020de7d7eda5fb31d19c22bf0d7e50
bonzaiferroni <lukas8@gmail.com>
2026-01-17 09:49:42 -0700

    bus locations fade

--- www/core/js/geoMap.js
+++ www/core/js/geoMap.js
@@ -4,10 +4,11 @@ document.addEventListener("DOMContentLoaded", initGeoMap)
 
 function initGeoMap() {
     geoMap = new maplibregl.Map({
-        container: "geoMap",
+        container: "geo-map",
         style: `https://tiles.openfreemap.org/styles/fiord`,
         center: [-104.95, 39.75],
         zoom: 11
     });
     geoMap.addControl(new maplibregl.NavigationControl(), "top-right");
+    geoMap.addControl(new maplibregl.FullscreenControl());
 }
