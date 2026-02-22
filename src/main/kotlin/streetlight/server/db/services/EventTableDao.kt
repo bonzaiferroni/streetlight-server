41a17542854460244b13305511ba09860d700fa4
bonzaiferroni <lukas8@gmail.com>
2026-02-22 14:59:14 -0700

    magic decoder

--- src/main/kotlin/streetlight/server/db/services/EventTableDao.kt
+++ src/main/kotlin/streetlight/server/db/services/EventTableDao.kt
@@ -1,26 +1,33 @@
 package streetlight.server.db.services
 
-import kampfire.api.isEmpty
+import kabinet.console.globalConsole
 import kampfire.model.UserId
 import klutch.db.DbService
 import klutch.db.deleteSingle
+import klutch.db.isNearEq
 import klutch.db.read
+import klutch.db.readFirstOrNull
 import klutch.db.updateSingleWhere
 import klutch.utils.eq
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.insertAndGetId
 import streetlight.model.data.Event
 import streetlight.model.data.EventId
 import streetlight.model.data.EventEdit
 import streetlight.model.data.EventStatus
 import streetlight.model.data.LocationId
 import streetlight.server.db.tables.EventTable
+import streetlight.server.db.tables.LocationTable
 import streetlight.server.db.tables.toEvent
+import streetlight.server.db.tables.toLocation
 import streetlight.server.db.tables.writeFull
 import streetlight.server.db.tables.writeUpdate
-import kotlin.time.Duration.Companion.hours
+import streetlight.server.utils.toProjectId
+
+private val console = globalConsole.getHandle(EventTableDao::class)
 
 class EventTableDao: DbService() {
     suspend fun readActiveEvents() = dbQuery {
@@ -34,7 +41,26 @@ class EventTableDao: DbService() {
 
     suspend fun createEvent(userId: UserId, event: EventEdit) = dbQuery {
         val now = Clock.System.now()
-        val locationId = event.locationId ?: return@dbQuery null
+        val initialLocationId = event.locationId
+        val locationId = if (initialLocationId != null) {
+            initialLocationId
+        } else {
+            val newLocation = event.newLocation
+            if (newLocation == null || !newLocation.isValid) return@dbQuery null
+            val location = LocationTable.readFirstOrNull {
+                it.name.eq(newLocation.name) and it.address.eq(newLocation.address) and it.geoPoint.isNearEq(newLocation.geoPoint)
+            }?.toLocation()
+
+            if (location != null) {
+                console.log("Using stored location ${location.name}")
+                location.locationId
+            } else {
+                console.log("creating new location ${newLocation.name} at ${newLocation.address}")
+                LocationTable.insertAndGetId {
+                    it.writeFull(newLocation.toLocation())
+                }.toProjectId()
+            }
+        }
         val event = event.toEvent(userId, locationId, now, now)
         EventTable.insert { it.writeFull(event) }
         event
