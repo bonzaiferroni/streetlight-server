5f90b601ccfc61ba2bdfa8577e4177e6fa7a4083
bonzaiferroni <lukas8@gmail.com>
2026-09-03 10:21:18 -0600

    refactored CreatePost

--- src/main/kotlin/streetlight/server/routes/serveMedia.kt
+++ src/main/kotlin/streetlight/server/routes/serveMedia.kt
@@ -1,8 +1,12 @@
 package streetlight.server.routes
 
+import kampfire.api.toSlug
+import kampfire.model.HttpProblem
+import kampfire.model.Ok
 import kampfire.model.toDataOr
 import kampfire.model.toOutcome
 import klutch.server.authGate
+import klutch.server.getApi
 import klutch.server.postApi
 import streetlight.model.Api
 import streetlight.server.db.tables.MediaTable
@@ -10,6 +14,15 @@ import streetlight.server.model.ApiScope
 import streetlight.server.model.getIdentity
 
 fun ApiScope.serveMedia() {
+
+    authGate(optional = true) {
+        getApi(Api.Medias.ReadMedia, { it.toSlug() }) {
+            val slug = it.data
+            HttpProblem.Conflict
+            // Ok(dao.media.readMedia(slug) ?: return@getApi HttpProblem.NotFound)
+        }
+    }
+
     authGate {
         postApi(Api.Medias.CreateMedia) { request ->
             val edit = request.data
